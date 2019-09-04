9347af2b2bb78873ca28ed6729a0b385519bf2af
Rob Lee <robert@gradem.io>
2019-09-03 23:13:49 -0400

    added in the relevant css

--- plugins/mathlive.js
+++ plugins/mathlive.js
@@ -1,4 +1,6 @@
 import MathLive from 'mathlive/dist/mathlive.js'
+import 'mathlive/dist/mathlive.core.css'
+import 'mathlive/dist/mathlive.css'
 
 export default ({ app }, inject) => {
   inject('mathlive', MathLive)
